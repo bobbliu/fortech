d91d0c395860fade37868282d6b78b90fa5265a5
bob <liubo.705@163.com>
2018-10-24 13:23:23 +0800

    Stack

--- java/src/main/java/com/liubo/base/datastructrue/stack/StackDemo.java
+++ java/src/main/java/com/liubo/base/datastructrue/stack/StackDemo.java
@@ -8,6 +8,21 @@ import java.util.Stack;
  * 栈是Vector的一个子类，它实现了一个标准的后进先出的栈。
  * <p>
  * 堆栈只定义了默认构造函数，用来创建一个空栈。 堆栈除了包括由Vector定义的所有方法，也定义了自己的一些方法。
+ *
+ * boolean empty()
+ * 测试堆栈是否为空。
+ *
+ * Object peek( )
+ * 查看堆栈顶部的对象，但不从堆栈中移除它。
+ *
+ * Object pop( )
+ * 移除堆栈顶部的对象，并作为此函数的值返回该对象。
+ *
+ * Object push(Object element)
+ * 把项压入堆栈顶部。
+ *
+ * nt search(Object element)
+ * 返回对象在堆栈中的位置，以 1 为基数。
  */
 public class StackDemo {
 
